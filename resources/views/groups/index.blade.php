57a36c17c793c6d908902ae12a6ccac921464463
David Lin <uylin40@gmail.com>
2021-01-24 20:55:52 +0800

    update

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -192,9 +192,6 @@
                     </thead>
                     <tbody>
                     @foreach($teacher_not_group as $group)
-                        <<<<<<< HEAD
-                        <tr data-id="{{ $group->id }}" data-number="{{ Config('map.class_number')[$group->grade] }}">
-                            =======
                         <?php
                         $number = 0;
                         if (array_key_exists($group->grade, Config('map.class_number'))) {
@@ -202,7 +199,6 @@
                         }
                         ?>
                         <tr data-id="{{ $group->id }}" data-number="{{ $number }}">
-                            >>>>>>> origin/dev
                             @if(Auth::user()->role >=50)
                                 <td>{{$group->g_id}}</td>
                             @endif
@@ -280,200 +276,155 @@
 @section('script')
     @parent
     <script>
-        $("#group_table_2").hide();
+        
+$("#group_table_2").hide();
+function sort_group_table(type){
+    var $tbody = $('#group_table tbody');
+	$tbody.find('tr').sort(function(a,b){ 
+		//var tda = $(a).find('td:eq(2)').text(); // can replace 1 with the column you want to sort on
+		//var tdb = $(b).find('td:eq(2)').text(); // this will sort on the second column
+		//var tda = $(a).find('td:eq(2)').data('number'); // can replace 1 with the column you want to sort on
+		//var tdb = $(b).find('td:eq(2)').data('number'); // this will sort on the second column
+		var tda = $(a).data('number'); // can replace 1 with the column you want to sort on
+		var tdb = $(b).data('number'); // this will sort on the second column
+				// if a < b return 1
+		if(type == 'desc'){
+			return tda < tdb ? 1 
+			   // else if a > b return -1
+			   : tda > tdb ? -1 
+			   // else they are equal - return 0    
+			   : 0;  
+		}else{
+			return tda > tdb ? 1 
+			   // else if a > b return -1
+			   : tda < tdb ? -1 
+			   // else they are equal - return 0    
+			   : 0;  
+		}
+		         
+	}).appendTo($tbody);
+	
+	var $tbody = $('#group_table_2 tbody');
+	$tbody.find('tr').sort(function(a,b){ 
+		//var tda = $(a).find('td:eq(2)').text(); // can replace 1 with the column you want to sort on
+		//var tdb = $(b).find('td:eq(2)').text(); // this will sort on the second column
+		//var tda = $(a).find('td:eq(2)').data('number'); // can replace 1 with the column you want to sort on
+		//var tdb = $(b).find('td:eq(2)').data('number'); // this will sort on the second column
+		var tda = $(a).data('number'); // can replace 1 with the column you want to sort on
+		var tdb = $(b).data('number'); // this will sort on the second column
+				// if a < b return 1
+		if(type == 'desc'){
+			return tda < tdb ? 1 
+			   // else if a > b return -1
+			   : tda > tdb ? -1 
+			   // else they are equal - return 0    
+			   : 0;  
+		}else{
+			return tda > tdb ? 1 
+			   // else if a > b return -1
+			   : tda < tdb ? -1 
+			   // else they are equal - return 0    
+			   : 0;  
+		}
+		         
+	}).appendTo($tbody);
+}
+//sort_group_table('desc');
 
-        function sort_group_table(id, type) {
-            var $tbody = $('#' + id + ' tbody');
-            $tbody.find('tr').sort(function (a, b) {
-                //var tda = $(a).find('td:eq(2)').text(); // can replace 1 with the column you want to sort on
-                //var tdb = $(b).find('td:eq(2)').text(); // this will sort on the second column
-                //var tda = $(a).find('td:eq(2)').data('number'); // can replace 1 with the column you want to sort on
-                //var tdb = $(b).find('td:eq(2)').data('number'); // this will sort on the second column
-                var tda = $(a).data('number'); // can replace 1 with the column you want to sort on
-                var tdb = $(b).data('number'); // this will sort on the second column
-            <<<<<<<
-                HEAD
-                === === =
-                // if a < b return 1
-                if (type == 'desc') {
-                    return tda < tdb ? 1
-                        // else if a > b return -1
-                        : tda > tdb ? -1
-                            // else they are equal - return 0
-                            : 0;
-                } else {
-                    return tda > tdb ? 1
-                        // else if a > b return -1
-                        : tda < tdb ? -1
-                            // else they are equal - return 0
-                            : 0;
-                }
-
-            }).appendTo($tbody);
-
-            var $tbody = $('#group_table_2 tbody');
-            $tbody.find('tr').sort(function (a, b) {
-                //var tda = $(a).find('td:eq(2)').text(); // can replace 1 with the column you want to sort on
-                //var tdb = $(b).find('td:eq(2)').text(); // this will sort on the second column
-                //var tda = $(a).find('td:eq(2)').data('number'); // can replace 1 with the column you want to sort on
-                //var tdb = $(b).find('td:eq(2)').data('number'); // this will sort on the second column
-                var tda = $(a).data('number'); // can replace 1 with the column you want to sort on
-                var tdb = $(b).data('number'); // this will sort on the second column
-            >>>>>>>
-                origin / dev
-                // if a < b return 1
-                if (type == 'desc') {
-                    return tda < tdb ? 1
-                        // else if a > b return -1
-                        : tda > tdb ? -1
-                            // else they are equal - return 0
-                            : 0;
-                } else {
-                    return tda > tdb ? 1
-                        // else if a > b return -1
-                        : tda < tdb ? -1
-                            // else they are equal - return 0
-                            : 0;
-                }
-
-            }).appendTo($tbody);
-        }
-
-        //sort_group_table('desc');
-
-        $("#sort_grade").change(function () {
-            var type = $(this).val();
-            sort_group_table('group_table', type);
-            sort_group_table('group_table_2', type);
-        });
+$("#sort_grade").change(function(){
+	var type = $(this).val();
+	sort_group_table(type);
+});
 
 
-        $("#filter_subject").change(function () {
-            //split the current value of searchInput
-            //sort_filter(this);
-            var data = this.value.split(" ");
-            //create a jquery object of the rows
-            var array = ['group_table', 'group_table_2'];
-            for (x in array) {
-                var jo = $('#' + array[x] + ' tbody').find("tr");
-                if (this.value == "") {
-                    jo.show();
-                    return;
-                }
-                //hide all the rows
-                jo.hide();
+$("#filter_subject").change(function () {
+    //split the current value of searchInput
+    var data = this.value.split(" ");
+    //create a jquery object of the rows
+    var jo = $('#group_table tbody').find("tr");
+    if (this.value == "") {
+        jo.show();
+        //return;
+    }
+    //hide all the rows
+    jo.hide();
 
-                //Recusively filter the jquery object to get results.
-                jo.filter(function (i, v) {
-                    var $t = $(this);
-                    for (var d = 0; d < data.length; ++d) {
-                        if ($t.is(":contains('" + data[d] + "')")) {
-                            return true;
-                        }
-                    }
-                    return false;
-                })
-                    //show the rows that match.
-                    .show();
-            }
-        });
-
-        function sort_filter(obj) {
-            var data = obj.value.split(" ");
-            //create a jquery object of the rows
-            var jo = $('#group_table tbody').find("tr");
-            if (obj.value == "") {
-                jo.show();
-                //return;
-            }
-            //hide all the rows
-            jo.hide();
-
-            //Recusively filter the jquery object to get results.
-            jo.filter(function (i, v) {
-                var $t = $(this);
-                for (var d = 0; d < data.length; ++d) {
-                    if ($t.is(":contains('" + data[d] + "')")) {
-                        return true;
-                    }
-                }
-                return false;
-            })
-                //show the rows that match.
-                .show();
-
-            var data = this.value.split(" ");
-            //create a jquery object of the rows
-            var jo = $('#group_table_2 tbody').find("tr");
-            if (this.value == "") {
-                jo.show();
-                //return;
-            }
-            //hide all the rows
-            jo.hide();
-
-            //Recusively filter the jquery object to get results.
-            jo.filter(function (i, v) {
-                var $t = $(obj);
-                for (var d = 0; d < data.length; ++d) {
-                    if ($t.is(":contains('" + data[d] + "')")) {
-                        return true;
-                    }
-                }
-                return false;
-            })
-                //show the rows that match.
-                .show();
-        <<<<<<<
-            HEAD
-        }
-
-        ======
-        =
-
-        })
-        ;
-        >>>>>>>
-        origin / dev
-
-        var group_classrooms = {!! json_encode($result,true) !!};
-
-        $(".assign").click(function () {
-            var group_id = $(this).closest('tr').data('id');
-            $('[name^="classroom_id"]').prop('checked', false);
-            $("#group_id").val(group_id);
-            for (x in group_classrooms[group_id]) {
-                $('[name^="classroom_id"][value="' + group_classrooms[group_id][x] + '"]').prop('checked', true);
-            }
-            $("#assignation_exam").modal('show');
-        });
-
-        /*
-        $("form").submit(function(e) {
-            e.preventDefault();
-            $.ajax({
-              type: 'POST',
-              url: $(this).attr('action'),
-              data: $("form").serialize(),
-            }).done(function(data) {
-              if(data.success){
-                  $("#teacher_signup").modal('show');
-              }else{
-                  alert(data.message);
-              }
-            });
-        });
-        */
-
-        function tab_show(num) {
-            if (num == 1) {
-                $("#group_table").show();
-                $("#group_table_2").hide();
-            } else {
-                $("#group_table").hide();
-                $("#group_table_2").show();
+    //Recusively filter the jquery object to get results.
+    jo.filter(function (i, v) {
+        var $t = $(this);
+        for (var d = 0; d < data.length; ++d) {
+            if ($t.is(":contains('" + data[d] + "')")) {
+                return true;
             }
         }
+        return false;
+    })
+    //show the rows that match.
+    .show();
+	
+	var data = this.value.split(" ");
+    //create a jquery object of the rows
+    var jo = $('#group_table_2 tbody').find("tr");
+    if (this.value == "") {
+        jo.show();
+        //return;
+    }
+    //hide all the rows
+    jo.hide();
+
+    //Recusively filter the jquery object to get results.
+    jo.filter(function (i, v) {
+        var $t = $(this);
+        for (var d = 0; d < data.length; ++d) {
+            if ($t.is(":contains('" + data[d] + "')")) {
+                return true;
+            }
+        }
+        return false;
+    })
+    //show the rows that match.
+    .show();
+	
+});
+
+var group_classrooms = [];
+
+$(".assign").click(function(){
+	var group_id = $(this).closest('tr').data('id');
+	$('[name^="classroom_id"]').prop('checked',false);
+	$("#group_id").val(group_id);
+	for(x in group_classrooms[group_id]){
+		$('[name^="classroom_id"][value="'+group_classrooms[group_id][x]+'"]').prop('checked',true);
+	}
+	$("#assignation_exam").modal('show');
+});
+/*
+$("form").submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+	  type: 'POST',
+	  url: $(this).attr('action'),
+	  data: $("form").serialize(),
+	}).done(function(data) {
+	  if(data.success){
+		  $("#teacher_signup").modal('show');
+	  }else{
+		  alert(data.message);
+	  }
+	});
+});
+*/
+
+function tab_show(num){
+	if(num == 1){
+		$("#group_table").show();
+		$("#group_table_2").hide();
+	}else{
+		$("#group_table").hide();
+		$("#group_table_2").show();
+	}
+}
+
 
     </script>
 @endsection
